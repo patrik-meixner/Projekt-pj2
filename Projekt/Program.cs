62a6075f24e73e651cfe407c48c231be42127a42
patrik_meixner <patrik.meixner98@gmail.com>
2020-03-31 19:24:06 +0200

    INIT

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace Projekt {
+﻿using System;
+
+namespace Projekt {
     internal class Program {
         public static void Main(string[] args) {
+            Console.Write("Pepik");
         }
     }
 }
